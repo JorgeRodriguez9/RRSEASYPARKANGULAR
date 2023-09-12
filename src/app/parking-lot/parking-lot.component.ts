dcc05a204ba63a163d94d31ce74d930ca7eba229
carlosandresramos <carlosandresramos803@gmail.com>
2023-09-11 22:24:19 -0500

    finish search

--- src/app/parking-lot/parking-lot.component.ts
+++ src/app/parking-lot/parking-lot.component.ts
@@ -6,7 +6,6 @@ import { City } from 'Models/City';
 import { CityService } from '../service/city/city.service';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-parking-lot',
@@ -22,10 +21,17 @@ export class ParkingLotComponent implements OnInit, AfterViewInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;//! le estamos diciendo que no es nulo
   //esta línea de código está configurando una referencia al paginador de Angular Material
   //para que pueda ser utilizado y manipulado en el componente donde se encuentra esta declaración
-  @ViewChild(MatSort) sort!: MatSort;
 
-  form: FormGroup;
-constructor(private _ParkingLotService:ParkingLotService, private _CityService:CityService, private formBuilder: FormBuilder){ 
+  cityform!: FormGroup;
+  form!: FormGroup;
+
+constructor(private _ParkingLotService:ParkingLotService, private _CityService:CityService, private formBuilder: FormBuilder, private fb: FormBuilder){ 
+  
+  this.cityform = this.fb.group({
+    selectedCity: [''],
+    filterValue: ['']
+  });
+
   this.form = this.formBuilder.group({
     name: [''],
     adress:[''],
@@ -73,7 +79,6 @@ ngOnInit(): void {
 }
 ngAfterViewInit() {
   this.dataSource.paginator = this.paginator;
-  this.dataSource.sort = this.sort;
 }
 getParkingLots(){
   this._ParkingLotService.getParkingLots().subscribe(response =>{ this.dataSource.data = response;});
@@ -81,8 +86,72 @@ getParkingLots(){
 getCity(){
   this._CityService.getCity().subscribe(response =>{ this.ListCities = response;});
 }
+/*
 applyFilter(event: Event) {
   const filterValue = (event.target as HTMLInputElement).value;
   this.dataSource.filter = filterValue.trim().toLowerCase();
+  this.dataSource.filterPredicate = (data, filter) => {
+    return data.adress.toLowerCase().includes(filter);
+  };
+
+  // La función filterPredicate toma dos argumentos: data (que representa un elemento de tu fuente de datos) 
+  //y filter (que es el valor que se está utilizando para filtrar).
+
+  //En este caso, la función de filtro personalizada verifica si el name de data incluye el filtro proporcionado. 
+}
+
+applyFilterCity(event: MatSelectChange) {
+  const selectedCity = event.value;
+
+  if (selectedCity) {
+    this.dataSource.filter = selectedCity.toString();
+    this.dataSource.filterPredicate = (data, filter) => {
+      return data.cityId.toString() === filter;
+    };
+  } else {
+    this.dataSource.filter = '';
+  }
+}
+*/
+applyCityFilter() {
+  const selectedCityControl = this.cityform.get('selectedCity');
+  const filterValueControl = this.cityform.get('filterValue');
+
+  if (selectedCityControl && filterValueControl) {
+    const selectedCity = selectedCityControl.value;
+    const filterValue = filterValueControl.value;
+
+    this.dataSource.filter = JSON.stringify({ selectedCity, filterValue });
+    //esta cogiendo selectedCity y filterValue, y los une y los convierte en una cadena de texto. Esto le dice al programa qué buscar en los datos.
+    //en resumen esta asignando como sera el filtro que necesita
+
+    this.dataSource.filterPredicate = (data, filters) => {
+    const { selectedCity, filterValue } = JSON.parse(filters);
+
+    //filterPredicate: aqui le decimos que muestre solo los objetos que cumplan con ciertas condiciones que yo extablesco
+    //filterPredicate se ejecuta automáticamente en cada dato cuando se aplica el filtro en la interfaz de usuario.
+    let cityFilter = true;
+    if (selectedCity) {
+      cityFilter = data.cityId.toString() === selectedCity.toString();
+    }
+
+    let textFilter = true;
+    if (filterValue) {
+      textFilter = data.adress.toLowerCase().includes(filterValue.toLowerCase());
+    }
+
+    return cityFilter && textFilter;
+
+  };
+  if (!filterValue) {
+    this.dataSource.filter = JSON.stringify({ selectedCity, filterValue: '' });
+  }
+  } else {
+    console.error('Alguno de los controles no está definido.');
+  }
+
+  
+
+  
 }
 }
