14cc713ca472e3568280d565fc5c37ece1dc61c3
JorgeRodriguez9 <131720274+JorgeRodriguez9@users.noreply.github.com>
2023-09-29 18:45:33 -0500

    End Principal Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ParkingLotComponent } from './components/parking-lot/parking-lot.component';
 import { ReservationComponent } from './components/reservation/reservation.component';
+import { PrincipalPageComponent } from './components/principal-page/principal-page.component';
 
 
 const routes: Routes = [
   {path: 'ParkingLot', component:ParkingLotComponent},
-  {path: 'Reservation/:id', component:ReservationComponent}
+  {path: 'Reservation/:id', component:ReservationComponent},
+  {path: 'PrincipalPage', component:PrincipalPageComponent}
 ];
 
 @NgModule({
