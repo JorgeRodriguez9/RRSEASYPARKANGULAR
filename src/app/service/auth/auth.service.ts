61d47631dafdc79f3ddc12d8270924c40e75a407
JorgeRodriguez9 <131720274+JorgeRodriguez9@users.noreply.github.com>
2023-10-26 02:01:29 -0500

    Setings

--- src/app/service/auth/auth.service.ts
+++ src/app/service/auth/auth.service.ts
@@ -20,76 +20,76 @@ export class AuthService {
 
 
 
-  public get userData(): User | null{
+  public get userData(): User | null {
     return this.userSubject.value;
   }
- // private userSubject = new BehaviorSubject<User>();
+  // private userSubject = new BehaviorSubject<User>();
   private userSubject = new BehaviorSubject<User | null>(null);
-  public useer! :Observable<User | null>;
+  public useer!: Observable<User | null>;
 
-  
 
-  constructor(private _http: HttpClient, private router : Router) { 
+
+  constructor(private _http: HttpClient, private router: Router) {
 
     const storedUser = localStorage.getItem('Usuario');
-/*
-    if (storedUser !== null) {
-      this.userSubject = new BehaviorSubject<User>(JSON.parse(storedUser));
-    } 
-    */
+    /*
+        if (storedUser !== null) {
+          this.userSubject = new BehaviorSubject<User>(JSON.parse(storedUser));
+        } 
+        */
     if (storedUser !== null) {
       const parsedUser = JSON.parse(storedUser);
       if (parsedUser) {
         this.userSubject = new BehaviorSubject<User | null>(parsedUser);
-      } 
-    } 
+      }
+    }
     this.useer = this.userSubject.asObservable();
   }
 
-    login(login: Login): Observable<Responses>{
+  login(login: Login): Observable<Responses> {
 
-      const Password = login.Password;
-      const nameUser = login.nameUser;
-      
-     return this._http.post<Responses>(this.Endpoint + this.complement,{nameUser, Password}).pipe(
+    const Password = login.Password;
+    const nameUser = login.nameUser;
+
+    return this._http.post<Responses>(this.Endpoint + this.complement, { nameUser, Password }).pipe(
       map(res => {
-        if(res.result == 0){
-          
-           const user: User = res.data;
-           localStorage.setItem('Usuario', JSON.stringify(user));
-           this.userSubject.next(user);
-           console.log(user);
+        if (res.result == 0) {
+
+          const user: User = res.data;
+          localStorage.setItem('Usuario', JSON.stringify(user));
+          this.userSubject.next(user);
+          console.log(user);
         }
         return res;
       })
-     );
-}
+    );
+  }
 
-getTokenUserInfo(): UserPost | null{
+  getTokenUserInfo(): UserPost | null {
 
-  const token = localStorage.getItem('Usuario');
+    const token = localStorage.getItem('Usuario');
 
-  if (token) {
-    const user = jwt_decode(token) as UserPost;
-    
-  console.log(user.Id); // Acceso al ID del usuario
-  console.log(user.Name); // Acceso al nombre del usuario
-  console.log(user.RolId); // Acceso al ID del rol del usuario
-    return user;
+    if (token) {
+      const user = jwt_decode(token) as UserPost;
+
+      console.log(user.Id); // Acceso al ID del usuario
+      console.log(user.Name); // Acceso al nombre del usuario
+      console.log(user.RolId); // Acceso al ID del rol del usuario
+      return user;
+    }
+
+
+    return null;
+
+  }
+
+  logout() {
+    localStorage.removeItem('Usuario');
+    this.userSubject.next(null);
+    this.router.navigate(['/Login']);
   }
 
 
-  return null;
-  
-}
 
-logout(){
-  localStorage.removeItem('Usuario');
-  this.userSubject.next(null);
-  this.router.navigate(['/Login']);
-}
-
-
-  
 
 }
